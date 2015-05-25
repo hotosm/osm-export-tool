6d7bb10fc48be8b4ea9d54d00fe7ecb0eeb7d4db
brian <bjohare@gmail.com>
2015-05-25 13:03:44 +0000

    Adding coverage script.

---
+++ cover.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# coverage integration with nose is broken: 
+# https://github.com/django-nose/django-nose/issues/2
+
+coverage run  --branch --source=jobs,api,tasks ./manage.py test 
+coverage report
+coverage html -d cover
+
