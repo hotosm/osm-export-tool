93a7b0d5071feaeca83aff23396ab24a87052c75
brian <bjohare@gmail.com>
2015-05-26 12:38:46 +0000

    Updated migrations

---
+++ jobs/migrations/0002_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('jobs', 'insert_export_formats'),
+        ('jobs', 'load_regions'),
+    ]
+
+    operations = [
+    ]
