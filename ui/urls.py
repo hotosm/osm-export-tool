1e1429abc57d7175ce0d053303f5e41556115eb4
brian <bjohare@gmail.com>
2015-06-26 17:01:34 +0000

    Adding static templates. Adding tabs to create page.

--- ui/urls.py
+++ ui/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('ui.views',
     url(r'^$', TemplateView.as_view(template_name='ui/list.html'), name='list'),
     url(r'^create/$', TemplateView.as_view(template_name='ui/create.html'), name='create'),
     url(r'^edit/$', TemplateView.as_view(template_name='ui/edit.html'), name='edit'),
+    url(r'^help/$', TemplateView.as_view(template_name='ui/edit.html'), name='edit'),
 )
