6f1680b2c5b39a33b1e35a4ecfed61f1b9104072
jduncan <jduncan@radiantblue.com>
2016-08-23 09:19:57 -0400

    initial work to convert to OL3 for the create page.

--- oet2/ui/static/ui/js/layers.js
+++ oet2/ui/static/ui/js/layers.js
@@ -3,15 +3,27 @@
  */
 var Layers = {};
 
-Layers.OSM = new OpenLayers.Layer.OSM("OpenStreetMap");
+Layers.OSM = new ol.layer.Tile({
+    title: "OpenStreetMap",
+    source: new ol.source.OSM()})
+//Layers.OSM = new OpenLayers.Layer.OSM("OpenStreetMap");
 
-Layers.HOT = new OpenLayers.Layer.XYZ("Humanitarian OSM",
-                ["//a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
-                 "//b.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
-                 "//c.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-                ],
-                {
-                    isBaseLayer: true,
-                    sphericalMercator: true,
-                }
-            );
+
+var layer = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+        url: ["//a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+            "//b.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+            "//c.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"]
+})
+});
+
+//Layers.HOT = new OpenLayers.Layer.XYZ("Humanitarian OSM",
+ //               ["//a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+ //                "//b.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+ //                "//c.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+ //               ],
+ //               {
+ //                   isBaseLayer: true,
+ //                   sphericalMercator: true,
+ //               }
+ //           );
