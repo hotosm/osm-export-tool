136a53a71612408be8eb52154fb2ed8bafbabfb0
Brandon Liu <bdon@bdon.org>
2017-03-02 14:27:22 -0500

    Revert "Fix for #86. Adding search by region name in admin."
    This reverts commit df84503b484b994f2a0c5183775a52e8d5ce7e88.

--- jobs/admin.py
+++ jobs/admin.py
@@ -22,7 +22,7 @@ class HOTRegionGeoAdmin(OSMGeoAdmin):
     model = Region
     exclude = ['the_geom', 'the_geog']
 
-    def save_model(self, request, obj, form, change):  # pragma no cover    
+    def save_model(self, request, obj, form, change):  # pragma no cover
         geom_merc = obj.the_geom_webmercator
         obj.the_geom = geom_merc.transform(ct=4326, clone=True)
         obj.the_geog = GEOSGeometry(obj.the_geom.wkt)
@@ -33,7 +33,7 @@ class JobAdmin(OSMGeoAdmin):
     """
     Admin model for editing Jobs in the admin interface.
     """
-    search_fields = ['uid', 'name', 'user__username', 'region__name']
+    search_fields = ['uid', 'name', 'user__username']
     list_display = ['uid', 'name', 'user', 'region']
     exclude = ['the_geom', 'the_geog']
     actions = ['select_exports']
