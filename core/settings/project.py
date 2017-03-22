27428651001c93bb2b54e744e2d2337dd119a449
Seth Fitzsimmons <seth@mojodna.net>
2017-03-22 09:38:23 -0700

    Typo

--- core/settings/project.py
+++ core/settings/project.py
@@ -215,7 +215,7 @@ LOGGING = {
 }
 
 EMAIL_HOST = os.environ.get('EMAIL_HOST', None)
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST', None)
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', None)
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', None)
 EMAIL_PORT = os.environ.get('EMAIL_PORT', None)
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', None)
