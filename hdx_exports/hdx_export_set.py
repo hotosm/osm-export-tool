8d6aa831c9de6d8539930c57dd0617c536c28632
Brandon Liu <bdon@bdon.org>
2017-03-30 17:21:29 -0400

    implementation of HDXExportSet

--- hdx_exports/hdx_export_set.py
+++ hdx_exports/hdx_export_set.py
@@ -8,63 +8,7 @@ from feature_selection import FeatureSelection
 from hdx.data.dataset import Dataset
 from hdx.configuration import Configuration
 
-class HDXExportSet(object):
-    """
-    An HDXExportSet is a set of remote resources (HDX datasets)
-    corresponding to a feature selection and area of interest.
-    This is a plain old python object that can be used independent of the web application.
-    """
-
-    def __init__(self,extent_path,feature_selection,base_slug,description,country_code):
-        self._extent_path = extent_path
-        self._feature_selection = feature_selection
-        self._base_slug = base_slug
-        self._description = description
-        self._country_code = country_code
-
-    @property
-    def bounds(self):
-        return self.clipping_polygon.bounds
-
-    @property
-    def base_slug(self):
-        return self._base_slug
-
-    @property
-    def clipping_polygon(self):
-        f = open(self._extent_path)
-        features = json.loads(f.read())['features']
-        assert len(features) == 1
-        theshape = shape(features[0]['geometry'])
-        theshape = theshape.buffer(0.01)
-        theshape = theshape.simplify(0.01)
-        return theshape
-        #with open('temp.geojson','w') as out:
-        #    out.write(json.dumps(mapping(theshape)))
-        
-
-    # has one hdx dataset per theme.
-    @property
-    def hdx_datasets(self): 
-        retval = []
-        for theme in self._feature_selection.themes:
-            retval.append(self._base_slug + '_' + theme)
-        return retval
-
-    def zip_resources(self,stage_root):
-        stage_dir = stage_root + h.base_slug + '/'
-        sqlite = stage_dir + h.base_slug + ".sqlite"
-        for table in self._feature_selection.tables:
-            subprocess.check_call('ogr2ogr -f "ESRI Shapefile" {0}{1}/{2}.shp {3} -lco ENCODING=UTF-8 -sql "select * from {2};"'.format(stage_root,self.base_slug,table,sqlite),shell=True,executable='/bin/bash')
-            exts = ['.shp','.dbf','.prj','.shx','.cpg']
-            with zipfile.ZipFile(stage_dir + h.base_slug + "_" + table + ".zip",'w',zipfile.ZIP_DEFLATED) as z:
-                for e in exts:
-                    z.write(stage_dir + table + e,table + e)
-            for e in exts:
-                os.remove(stage_dir + table + e)
-
-    def hdx_note(self,theme):
-        MARKDOWN = '''
+MARKDOWN = '''
 Shapefiles of [OpenStreetMap](http://www.openstreetmap.org) features in {region}.
 
 The shapefiles include all OpenStreetMap features matching:
@@ -79,22 +23,53 @@ This dataset is one of many [OpenStreetMap exports on HDX](https://data.humdata.
 See the [Humanitarian OpenStreetMap Team](http://hotosm.org/) website for more information.
 '''
 
-        columns = []
-        for key in self._feature_selection.key_selections(theme):
-            columns.append("- [{0}](http://wiki.openstreetmap.org/wiki/Key:{0})".format(key))
-        columns = "\n".join(columns)
-        return MARKDOWN.format(
-            region = self._description,
-            criteria = self._feature_selection.filter_clause(theme),
-            columns = columns
-        )
+class HDXExportSet(object):
+    """
+    An HDXExportSet is a set of remote resources (HDX datasets)
+    corresponding to a feature selection and area of interest.
+    This is a plain old python object that can be used independent of the web application.
+    """
 
-    def sync_datasets(self):
+    def __init__(self,dataset_prefix,name,extent,feature_selection,country_codes=[]):
+        # raise exceptions on invalid feature selections, extents.
+        # it's not the job of this class to validate those!
+        assert extent['type'] in ['Polygon','MultiPolygon']
+        self._extent = extent
+        self._feature_selection = feature_selection
+        self._dataset_prefix = dataset_prefix
+        self._name = name
+        self._country_codes = country_codes
+        self._datasets = None
+
+    @property
+    def country_codes(self):
+        return self._country_codes
+
+    @property
+    def bounds(self):
+        return self.clipping_polygon.bounds
+
+    @property
+    def clipping_polygon(self):
+        f = open(self._extent_path)
+        features = json.loads(f.read())['features']
+        assert len(features) == 1
+        theshape = shape(features[0]['geometry'])
+        theshape = theshape.buffer(0.01)
+        theshape = theshape.simplify(0.01)
+        return theshape
+
+    @property
+    def datasets(self): 
+        if self._datasets:
+            return self._datasets
+
+        self._datasets = []
         for theme in self._feature_selection.themes:
             dataset = Dataset()
-            name = self._base_slug + "_" + theme
+            name = self._dataset_prefix + "_" + theme
             dataset['name'] = name
-            dataset['title'] = self._description + ' ' + theme + ' (OpenStreetMap Export)'
+            dataset['title'] = self._name + ' ' + theme + ' (OpenStreetMap Export)'
             dataset['private'] = True
             dataset['notes'] = self.hdx_note(theme)
             dataset['dataset_source'] = 'OpenStreetMap'
@@ -105,8 +80,46 @@ See the [Humanitarian OpenStreetMap Team](http://hotosm.org/) website for more i
             dataset['methodology_other'] = 'OpenStreetMap extract'
             dataset['data_update_frequency'] = '7'
             dataset['subnational'] = '1'
-            dataset.add_country_location(self._country_code)
+            for country_code in self._country_codes:
+                dataset.add_country_location(country_code)
+            self._datasets.append(dataset)
+        return self._datasets
 
+
+    def hdx_note(self,theme):
+        columns = []
+        for key in self._feature_selection.key_selections(theme):
+            columns.append("- [{0}](http://wiki.openstreetmap.org/wiki/Key:{0})".format(key))
+        columns = "\n".join(columns)
+        return MARKDOWN.format(
+            region = self._name,
+            criteria = self._feature_selection.filter_clause(theme),
+            columns = columns
+        )
+
+    def sync_datasets(self):
+        for dataset in self.datasets:
+            exists = Dataset.read_from_hdx(dataset['name'])
+            if exists:
+                dataset.update_in_hdx()
+            else:
+                dataset.create_in_hdx()
+
+    # will get to these later...
+
+    def zip_resources(self,stage_root):
+        stage_dir = stage_root + h.base_slug + '/'
+        sqlite = stage_dir + h.base_slug + ".sqlite"
+        for table in self._feature_selection.tables:
+            subprocess.check_call('ogr2ogr -f "ESRI Shapefile" {0}{1}/{2}.shp {3} -lco ENCODING=UTF-8 -sql "select * from {2};"'.format(stage_root,self.base_slug,table,sqlite),shell=True,executable='/bin/bash')
+            exts = ['.shp','.dbf','.prj','.shx','.cpg']
+            with zipfile.ZipFile(stage_dir + h.base_slug + "_" + table + ".zip",'w',zipfile.ZIP_DEFLATED) as z:
+                for e in exts:
+                    z.write(stage_dir + table + e,table + e)
+            for e in exts:
+                os.remove(stage_dir + table + e)
+
+    def sync_resources(self):
             resources = []
             for geom_type in self._feature_selection.geom_types(theme):
                 resource_name = theme + '_' + geom_type # DRY me up
@@ -117,17 +130,12 @@ See the [Humanitarian OpenStreetMap Team](http://hotosm.org/) website for more i
                     'description': "ESRI Shapefile of " + geom_type
                 })
             dataset.add_update_resources(resources)
-            d = Dataset.read_from_hdx(name)
-            if d:
-                dataset.update_in_hdx()
-            else:
-                dataset.create_in_hdx()
 
 
 if __name__ == "__main__":
-    f_s = FeatureSelection(open('hdx/example_preset.yml').read())
-    h = HDXExportSet('hdx/adm0/GIN_adm0.geojson',f_s,'hotosm_guinea','Guinea','GIN')
-    #h = HDXExportSet('hdx/adm0/SEN_adm0.geojson',f_s,'hotosm_senegal','Senegal','SEN')
+    f_s = FeatureSelection(open('hdx_exports/example_preset.yml').read())
+    extent = json.loads(open('hdx_exports/adm0/GIN_adm0.geojson').read())
+    h = HDXExportSet('hotosm_guinea','Guinea',extent,f_s,country_codes=['GIN'])
     #h.zip_resources("../stage/")
-    Configuration.create(hdx_site='prod',hdx_key=os.environ['HDXKEY'])
+    Configuration.create(hdx_site='prod',hdx_key=os.environ['HDX_API_KEY'])
     h.sync_datasets()
---
+++ hdx_exports/tests/test_hdx_export_set.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from hdx_exports.hdx_export_set import HDXExportSet
+from hdx_exports.feature_selection import FeatureSelection
+
+from hdx.data.hdxobject import HDXError
+
+DAKAR_GEOJSON_POLYGON = {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [-17.465,14.719],
+            [-17.442,14.719],
+            [-17.442,14.741],
+            [-17.465,14.741],
+            [-17.465,14.719]
+          ]
+        ]
+      }
+
+DAKAR_GEOJSON_MULTIPOLYGON = {
+        "type": "MultiPolygon",
+        "coordinates": [[
+          [
+            [-17.465,14.719],
+            [-17.442,14.719],
+            [-17.442,14.741],
+            [-17.465,14.741],
+            [-17.465,14.719]
+          ]]
+        ]
+      }
+
+yaml = '''
+buildings:
+    types:
+        - polygons
+    select:
+        - building
+    where: building is not null
+
+waterways:
+    types:
+        - lines
+        - polygons
+    select:
+        - natural
+    where: natural in ('waterway')
+'''
+BASIC_FEATURE_SELECTION = FeatureSelection(yaml)
+
+class TestHDXExportSet(unittest.TestCase):
+
+    def test_minimal_export_set(self):
+
+        h = HDXExportSet(
+            "hot_dakar",
+            "Dakar Urban Area",
+            DAKAR_GEOJSON_POLYGON, # or multipolygon. maybe this should be Shapely geom instead of dict?
+            BASIC_FEATURE_SELECTION
+        )
+        self.assertEquals([],h.country_codes)
+        datasets = h.datasets
+        self.assertEquals(len(datasets),2)
+        self.assertEquals(datasets[0]['name'],'hot_dakar_buildings')
+
+    def test_extent_not_polygon_or_multipolygon(self):
+        with self.assertRaises(AssertionError):
+            h = HDXExportSet(
+                "hot_dakar",
+                "Dakar Urban Area",
+                {'type':'FeatureCollection'},
+                BASIC_FEATURE_SELECTION 
+            )
+
+    def test_invalid_country_codes(self):
+        h = HDXExportSet(
+            "hot_dakar",
+            "Dakar Urban Area",
+            DAKAR_GEOJSON_POLYGON,
+            BASIC_FEATURE_SELECTION,
+            country_codes=['XXX']
+        )
+        with self.assertRaises(HDXError):
+            h.datasets
+    
