973e008c8e94dab7fa71990b20fc91cd8dd48bd8
Seth Fitzsimmons <seth@mojodna.net>
2017-03-30 15:34:39 -0700

    Wired up HDX edit form to server-side validation

--- ui/app/hdx/components/HDXCreateForm.jsx
+++ ui/app/hdx/components/HDXCreateForm.jsx
@@ -1,67 +1,105 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-import { connect } from 'react-redux';
-import { FormGroup, ControlLabel, FormControl, HelpBlock, Row, Col, Checkbox, Panel, Button } from 'react-bootstrap';
+
 import axios from 'axios';
+import { FormGroup, ControlLabel, FormControl, HelpBlock, Row, Col, Checkbox, Panel, Button } from 'react-bootstrap';
 import cookie from 'react-cookie';
+import { connect } from 'react-redux';
+import { Field, SubmissionError, formValueSelector, reduxForm } from 'redux-form';
 
 import styles from '../styles/HDXCreateForm.css';
 
-const AVAILABLE_EXPORT_FORMATS = ['shp', 'geopackage', 'garmin', 'kml', 'pbf'];
+const AVAILABLE_EXPORT_FORMATS = {
+  shp: 'ESRI Shapefiles',
+  geopackage: 'GeoPackage',
+  garmin: 'Garmin .IMG',
+  kml: '.KMZ',
+  pbf: 'OpenStreetMap .PBF'
+};
 
 const form = reduxForm({
   form: 'HDXCreateForm',
   onSubmit: values => {
     console.log('Submitting form. Values:', values);
-    const csrfmiddlewaretoken = cookie.load('csrftoken');
+    if (values.aoiInfo.geomType == null) {
+      throw new SubmissionError({
+        _error: 'Please select an area of interest →'
+      });
+    }
 
-    const exportFormats = AVAILABLE_EXPORT_FORMATS.filter(x => values[x]);
+    const exportFormats = Object.keys(AVAILABLE_EXPORT_FORMATS).filter(x => values[x]);
 
-    const formData = {};
-    Object.assign(formData, values, {
+    if (exportFormats.length === 0) {
+      throw new SubmissionError({
+        export_formats: 'At least one data format is required.'
+      });
+    }
+
+    const formData = {
+      ...values,
       the_geom: values.aoiInfo.geojson.features[0].geometry,
       export_formats: exportFormats
-    });
+    };
 
-    axios({
+    return axios({
       url: '/api/hdx_export_regions',
       method: 'POST',
       contentType: 'application/json; version=1.0',
       data: formData,
       headers: {
-        'X-CSRFToken': csrfmiddlewaretoken
+        'X-CSRFToken': cookie.load('csrftoken')
       }
-    }).then((response) => {
+    }).then(rsp => {
       console.log('Success');
-    }).catch((error) => {
-      if (error.response) {
-        console.log(error.response.data);
+
+      // TODO do something
+    }).catch(err => {
+      if (err.response) {
+        throw new SubmissionError(err.response.data);
       }
+
+      console.warn(err);
+
+      throw new SubmissionError({
+        _error: 'Export region creation failed.'
+      });
     });
   }
 });
 
-const FieldGroup = ({ id, label, help, ...props }) =>
-  <FormGroup controlId={id}>
+const renderInput = ({ id, input, label, help, meta: { touched, error }, ...props }) =>
+  <FormGroup controlId={id || props.name} validationState={error && 'error'}>
     <ControlLabel>{label}</ControlLabel>
-    <FormControl {...props} />
-    {help && <HelpBlock>{help}</HelpBlock>}
+    <FormControl {...input} {...props} />
+    <FormControl.Feedback />
+    <HelpBlock>{touched && error && <p className={styles.error}>{error}</p>}{help}</HelpBlock>
   </FormGroup>;
 
-const renderFieldGroup = ({input, data, meta, ...props}) =>
-  <FieldGroup {...input} {...props} />;
+const renderTextArea = ({id, label, input, data, meta: { touched, error }, ...props}) =>
+  <FormGroup controlId={id || input.name} validationState={error && 'error'}>
+    <ControlLabel>{label}</ControlLabel>
+    <FormControl componentClass='textarea' {...input} {...props} />
+    <FormControl.Feedback />
+    <HelpBlock>{touched && error && <span className={styles.error}>{error}</span>}</HelpBlock>
+  </FormGroup>;
 
-const renderTextArea = ({input, data, meta, ...props}) =>
-  <FormControl componentClass='textarea' {...input} {...props} />;
+const renderSelect = ({id, label, input, data, meta: { touched, error }, ...props}) =>
+  <FormGroup controlId={id || input.name} validationState={error && 'error'}>
+    <ControlLabel>{label}</ControlLabel>
+    <FormControl componentClass='select' {...input} {...props} />
+    <FormControl.Feedback />
+    <HelpBlock>{touched && error && <span className={styles.error}>{error}</span>}</HelpBlock>
+  </FormGroup>;
 
-const renderScheduleSelect = ({input, data, meta, ...props}) =>
-  <FormControl componentClass='select' {...input} {...props}>
-    <option value='day'>Daily</option>
-    <option value='week'>Weekly (Sunday)</option>
-    <option value='month'>Monthly (1st of month)</option>
-    <option value='6hours'>Every 6 hours</option>
-    <option value='never'>Don't automatically schedule</option>
-  </FormControl>;
+const renderCheckboxes = ({id, label, input, data, meta: { touched, error }, description, ...props}) =>
+  <FormGroup controlId={id || input.name} validationState={error && 'error'}>
+    <ControlLabel>{label}</ControlLabel>
+    {props.children}
+    <FormControl.Feedback />
+    <HelpBlock>{touched && error && <span className={styles.error}>{error}</span>}</HelpBlock>
+  </FormGroup>;
+
+const renderCheckbox = ({input, data, meta: { touched, error }, description, ...props}) =>
+  <Checkbox {...input} {...props}>{description}</Checkbox>;
 
 const getTimeOptions = () => {
   const options = [];
@@ -73,13 +111,14 @@ const getTimeOptions = () => {
   return options;
 };
 
-const renderTimeSelect = ({input, data, meta, ...props}) =>
-  <FormControl componentClass='select' {...input} {...props}>
-    { getTimeOptions() }
-  </FormControl>;
-
-const renderCheckbox = ({input, data, meta, description, ...props}) =>
-  <Checkbox {...input} {...props}>{description}</Checkbox>;
+const getFormatCheckboxes = () =>
+  Object.keys(AVAILABLE_EXPORT_FORMATS).map((k, i) =>
+    <Field
+      key={i}
+      name={k}
+      description={AVAILABLE_EXPORT_FORMATS[k]}
+      component={renderCheckbox}
+    />);
 
 export class HDXCreateForm extends Component {
   componentWillReceiveProps (props) {
@@ -87,94 +126,84 @@ export class HDXCreateForm extends Component {
   }
 
   render () {
-    const { handleSubmit, pristine, submitting } = this.props;
+    const { error, handleSubmit, submitting } = this.props;
+    let datasetPrefix = this.props.datasetPrefix || '<prefix>';
 
     return (
       <div className={styles.hdxCreateForm}>
         <form onSubmit={handleSubmit}>
           <h2>Create Export Region</h2>
+          {error && <strong className={styles.error}>{error}</strong>}
           <Field
-            id='formControlsText'
+            name='name'
+            type='text'
+            label='Dataset Name'
+            placeholder='Senegal'
+            component={renderInput}
+          />
+          <hr />
+          <Field
             name='dataset_prefix'
             type='text'
             label='Dataset Prefix'
             placeholder='hotosm_senegal'
-            component={renderFieldGroup}
+            help={<div>Example: prefix <code>hotosm_senegal</code> results in datasets <code>hotosm_senegal_roads</code>, <code>hotosm_senegal_buildings</code>, etc.</div>}
+            component={renderInput}
           />
-          <div>Example: prefix <code>hotosm_senegal</code> results in datasets <code>hotosm_senegal_roads</code>, <code>hotosm_senegal_buildings</code>, etc.</div>
           <hr />
-          <FormGroup controlId='formControlsTextarea'>
-            <ControlLabel>Feature Selection</ControlLabel>
-            <Field
-              rows='10'
-              name='feature_selection'
-              component={renderTextArea}
-            />
-          </FormGroup>
+          <Field
+            id='formControlsTextarea'
+            label='Feature Selection'
+            rows='10'
+            name='feature_selection'
+            component={renderTextArea}
+          />
           <hr />
           <Row>
             <Col xs={6}>
-              <FormGroup controlId='formControlsSelect'>
-                <ControlLabel>Run this export on an automated schedule:</ControlLabel>
-                <Field
-                  name='schedule_period'
-                  component={renderScheduleSelect}
-                />
-              </FormGroup>
+              <Field
+                name='schedule_period'
+                label='Run this export on an automated schedule:'
+                component={renderSelect}
+              >
+                <option value='day'>Daily</option>
+                <option value='week'>Weekly (Sunday)</option>
+                <option value='month'>Monthly (1st of month)</option>
+                <option value='6hours'>Every 6 hours</option>
+                <option value='never'>Don't automatically schedule</option>
+              </Field>
             </Col>
             <Col xs={5} xsOffset={1}>
-              <FormGroup controlId='formControlsSelect'>
-                <ControlLabel>At time:</ControlLabel>
-                <Field
-                  name='schedule_hour'
-                  component={renderTimeSelect}
-                />
-              </FormGroup>
+              <Field
+                name='schedule_hour'
+                label='At time:'
+                component={renderSelect}
+              >
+                { getTimeOptions() }
+              </Field>
             </Col>
           </Row>
           <Row>
             <Col xs={5}>
-              <FormGroup controlId='formControlsFormats'>
-                {/* TODO use AVAILABLE_EXPORT_FORMATS */}
-                <ControlLabel>File Formats</ControlLabel>
-                <Field
-                  name='shp'
-                  description='ESRI Shapefiles'
-                  component={renderCheckbox}
-                />
-                <Field
-                  name='geopackage'
-                  description='GeoPackage'
-                  component={renderCheckbox}
-                />
-                <Field
-                  name='garmin'
-                  description='Garmin .IMG'
-                  component={renderCheckbox}
-                />
-                <Field
-                  name='kml'
-                  description='.KMZ'
-                  component={renderCheckbox}
-                />
-                <Field
-                  name='pbf'
-                  description='OpenStreetMap .PBF'
-                  component={renderCheckbox}
-                />
-              </FormGroup>
+              <Field
+                name='export_formats'
+                label='File Formats'
+                component={renderCheckboxes}
+              >
+                {getFormatCheckboxes()}
+              </Field>
             </Col>
             <Col xs={7}>
               <Panel>
                 This will immediately create 5 datasets on HDX:
                 <ul>
-                  <li><code>hotosm_senegal_admin_boundaries</code></li>
-                  <li><code>hotosm_senegal_buildings</code></li>
-                  <li><code>hotosm_senegal_points_of_interest</code></li>
-                  <li><code>hotosm_senegal_roads</code></li>
-                  <li><code>hotosm_senegal_waterways</code></li>
+                  <li><code>{datasetPrefix}_admin_boundaries</code></li>
+                  <li><code>{datasetPrefix}_buildings</code></li>
+                  <li><code>{datasetPrefix}_points_of_interest</code></li>
+                  <li><code>{datasetPrefix}_roads</code></li>
+                  <li><code>{datasetPrefix}_waterways</code></li>
                 </ul>
-                <Button bsStyle='primary' bsSize='large' type='submit' disabled={pristine || submitting} onClick={handleSubmit} block>
+                <Button bsStyle='primary' bsSize='large' type='submit' disabled={submitting} onClick={handleSubmit} block>
                   Create Datasets + Run Export
                 </Button>
               </Panel>
@@ -191,7 +220,8 @@ const mapStateToProps = state => {
     initialValues: {
       aoiInfo: state.aoiInfo
     },
-    aoiInfo: state.aoiInfo
+    aoiInfo: state.aoiInfo,
+    datasetPrefix: formValueSelector('HDXCreateForm')(state, 'dataset_prefix')
   };
 };
 
