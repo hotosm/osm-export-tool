38c9cea1504b2ec918497a33bcc385d9eac12c6f
Brandon Liu <bdon@bdon.org>
2017-04-03 20:12:11 -0400

    basic implementation of SQL parsing in feature selection

--- feature_selection/feature_selection.py
+++ feature_selection/feature_selection.py
@@ -1,4 +1,8 @@
+import sqlparse
+from sqlparse.tokens import Token
 import yaml
+from yaml.constructor import ConstructorError
+from yaml.scanner import ScannerError
 
 CREATE_TEMPLATE = 'CREATE TABLE {0} AS SELECT Geometry,osm_id,{1} FROM {2} WHERE ({3}) AND ST_Contains(GeomFromText(?),Geometry);'
 INDEX_TEMPLATE = "SELECT RecoverGeometryColumn('{0}', 'GEOMETRY', 4326, '{1}', 'XY')"
@@ -15,30 +19,139 @@ OGR2OGR_TABLENAMES = {
     'polygons':'planet_osm_polygon'
 }
 
+class SQLValidator(object):
+    def __init__(self,raw_sql):
+        self._raw_sql = raw_sql
+        self._sql = None
+        self._errors = []
+
+    @property
+    def sql(self):
+        if self._sql:
+            return self._sql
+        if not self._raw_sql:
+            self._errors.append(["SQL clause is empty"])
+            return None
+
+        parsed = sqlparse.parse(self._raw_sql)
+
+        def is_valid_identifier(value):
+            print value
+            return True
+
+        
+        def is_whitelisted(token):
+            print token
+            print token.ttype
+            if isinstance(token,sqlparse.sql.Identifier):
+                return is_valid_identifier(token.value)
+            if isinstance(token,sqlparse.sql.IdentifierList):
+                x = True
+                for v in token.tokens:
+                    x = (x and is_valid_identifier(v))
+                    return x
+            if isinstance(token,sqlparse.sql.Comparison):
+                left = is_whitelisted(token.left)
+                right = is_whitelisted(token.right)
+                return (left and right)
+            if isinstance(token,sqlparse.sql.Parenthesis):
+                x = True
+                for t in token.tokens:
+                    x = (x and is_whitelisted(t))
+                return x
+            if token.ttype in [
+                Token.Keyword,
+                Token.Text.Whitespace,
+                Token.Operator.Comparison,
+                Token.Literal.String.Single,
+                Token.Literal.Number.Integer,
+                Token.Punctuation
+                ]:
+                return True
+            return False
+
+        for statement in parsed:
+            for token in statement.tokens:
+                if not is_whitelisted(token):
+                    self._errors.append("SQL Invalid")
+                    return None
+        self._sql = self._raw_sql
+        return self._sql
+
+
+    @property
+    def valid(self):
+        return not (self.sql == None)
+
+    @property
+    def errors(self):
+        return ""
+
+class InvalidFeatureSelectionException(Exception):
+    pass
+
+class ValidationErrorRepr(object):
+    def __init__(self,error):
+        self._error = error
+
+    def to_dict(self):
+        pass
+
 # FeatureSelection seralizes as YAML.
 # It describes a set of tables (themes)
 # to create in a Spatialite database.
 class FeatureSelection(object):
-    def __init__(self,doc):
-        self._yaml = yaml.load(doc)
-        self._valid = (self._yaml != None)
+    def __init__(self,raw_doc):
+        self._raw_doc = raw_doc
+        self._doc = None
+        self._errors = []
+
 
     @property
-    def themes(self):
-        return self._yaml.keys()
+    def doc(self):
+        def validate_schema(loaded_doc):
+            if not isinstance(loaded_doc,dict):
+                self._errors.append("YAML must be dict, not list")
+                return False
+            return True
+
+        if self._doc:
+            return self._doc
+        try:
+            loaded_doc = yaml.safe_load(self._raw_doc)
+            if validate_schema(loaded_doc):
+                self._doc = loaded_doc
+                return self._doc
+        except (ConstructorError,ScannerError) as e:
+            line = e.problem_mark.line
+            column = e.problem_mark.column
+            #print e.problem_mark.buffer
+            #print e.problem
+            self._errors.append(e.problem)
+            # add exceptions
+            #self._valid = (self._yaml != None)
+
 
     @property
     def valid(self):
-        return self._valid
+        return self.doc != None
+
+    @property
+    def errors(self):
+        return self._errors
+
+    @property
+    def themes(self):
+        return self.doc.keys()
 
     def geom_types(self,theme):
-        return self._yaml[theme]['types']
+        return self.doc[theme]['types']
 
     def key_selections(self,theme):
-        return self._yaml[theme]['select']
+        return self.doc[theme]['select']
 
     def filter_clause(self,theme):
-        theme = self._yaml[theme]
+        theme = self.doc[theme]
         if 'where' in theme:
             return theme['where']
         return '1'
--- feature_selection/tests/test_feature_selection.py
+++ feature_selection/tests/test_feature_selection.py
@@ -1,7 +1,39 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from feature_selection.feature_selection import FeatureSelection
+import yaml
+from feature_selection.feature_selection import FeatureSelection, SQLValidator
+
+class TestSQLValidator(unittest.TestCase):
+    def test_empty_sql(self):
+        s = SQLValidator("")
+        self.assertFalse(s.valid)
+
+    @unittest.skip("temporarily disabled")
+    def test_minimal(self):
+        s = SQLValidator("name IS NOT NULL")
+        self.assertTrue(s.valid)
+
+    @unittest.skip("temporarily disabled")
+    def test_malicious_sql(self):
+        s = SQLValidator("name IS NOT NULL; drop table planet_osm_polygon;")
+        self.assertFalse(s.valid)
+        s = SQLValidator("(drop table planet_osm_polygon)")
+        self.assertFalse(s.valid)
+        s = SQLValidator("(drop table planet_osm_polygon) > 0") # integer?
+        self.assertFalse(s.valid)
+
+    def test_conditions(self):
+        s = SQLValidator("natural = 'water'")
+        self.assertTrue(s.valid)
+        s = SQLValidator("admin IS NOT NULL AND admin:level = '4'")
+        self.assertTrue(s.valid)
+        s = SQLValidator("(admin IS NOT NULL AND admin:level = '4')")
+        self.assertTrue(s.valid)
+        s = SQLValidator("natural in ('water','park')")
+        self.assertTrue(s.valid)
+        s = SQLValidator("height > 0")
+        self.assertTrue(s.valid)
 
 class TestFeatureSelection(unittest.TestCase):
 
@@ -11,64 +43,7 @@ class TestFeatureSelection(unittest.TestCase):
         f = FeatureSelection(y)
         self.assertFalse(f.valid)
 
-    def test_one_theme(self):
-        y = '''
-        buildings:
-            types: 
-                - polygons
-        '''
-        f = FeatureSelection(y)
-        self.assertEquals(f.themes,['buildings'])
-
-    def test_theme_geom_types(self):
-        y = '''
-        waterways:
-            types: 
-                - lines
-                - polygons
-        '''
-        f = FeatureSelection(y)
-        self.assertEquals(f.geom_types('waterways'),['lines','polygons'])
-
-    def test_theme_key_selections(self):
-        y = '''
-        waterways:
-            types: 
-                - lines
-                - polygons
-            select:
-                - osm_id
-                - name
-        '''
-        f = FeatureSelection(y)
-        self.assertEquals(f.key_selections('waterways'),['osm_id','name'])
-
-    def test_theme_filter(self):
-        y = '''
-        waterways:
-            types: 
-                - lines
-                - polygons
-            select:
-                - osm_id
-                - name
-        '''
-        f = FeatureSelection(y)
-        self.assertEquals(f.filter_clause('waterways'),'1') # SELECT WHERE 1
-        y = '''
-        waterways:
-            types: 
-                - lines
-                - polygons
-            select:
-                - osm_id
-                - name
-            where: waterway IS NOT NULL
-        '''
-        f = FeatureSelection(y)
-        self.assertEquals(f.filter_clause('waterways'),'waterway IS NOT NULL')
-
-    def test_key_union(self):
+    def test_key_union_and_filters(self):
         y = '''
         waterways:
             types: 
@@ -86,10 +61,17 @@ class TestFeatureSelection(unittest.TestCase):
                 - osm_id
                 - name
                 - building
+            where: building IS NOT NULL
         '''
         f = FeatureSelection(y)
+        self.assertEquals(f.themes,['buildings','waterways'])
+        self.assertEquals(f.geom_types('waterways'),['lines','polygons'])
+        self.assertEquals(f.key_selections('waterways'),['osm_id','name','waterway'])
+        self.assertEquals(f.filter_clause('waterways'),'1') # SELECT WHERE 1
         self.assertEquals(f.key_union, ['building','name','osm_id','waterway'])
+        self.assertEquals(f.filter_clause('buildings'),'building IS NOT NULL')
 
+    @unittest.skip("temporarily disabled")
     def test_sqls(self):
         y = '''
         waterways:
@@ -112,3 +94,43 @@ class TestFeatureSelection(unittest.TestCase):
         f = FeatureSelection(y)
         print f.sqls
 
+    def test_unsafe_yaml(self):
+        y = '''
+        !!python/object:feature_selection.feature_selection.FeatureSelection
+        a: 0
+        '''
+        f = FeatureSelection(y)
+        self.assertFalse(f.valid)
+        self.assertEqual(1,len(f.errors))
+
+    def test_malformed_yaml(self):
+        # if it's not a valid YAML document
+        # TODO: errors for if yaml indentation is incorrect
+        y = '''
+        all
+            select:
+                - name
+        '''
+        f = FeatureSelection(y)
+        self.assertFalse(f.valid)
+
+    def test_minimal_yaml(self):
+        # the shortest valid feature selection
+        y = '''
+        all: 
+            select:
+                - name
+        '''
+        f = FeatureSelection(y)
+        self.assertTrue(f.valid)
+
+    def test_unspecified_yaml(self):
+        # top level is a list and not a dict
+        y = '''
+        - all: 
+            select:
+                - name
+        '''
+        f = FeatureSelection(y)
+        self.assertFalse(f.valid)
+        self.assertEqual(f.errors[0],"YAML must be dict, not list")
