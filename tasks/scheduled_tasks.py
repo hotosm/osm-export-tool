b0439805ab95f116fc306dde0ec7a55407825379
Seth Fitzsimmons <seth@mojodna.net>
2017-04-28 12:07:51 -0700

    Only run scheduled exports once during their hour

--- tasks/scheduled_tasks.py
+++ tasks/scheduled_tasks.py
@@ -1,7 +1,7 @@
 # noqa
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.utils import timezone
 import pytz
@@ -23,15 +23,22 @@ def queue_periodic_job_runs(): # noqa
         else:
             last_run_at = datetime.fromtimestamp(0, pytz.timezone('UTC'))
 
-        if (now - last_run_at > region.delta) or \
+        delta = now - last_run_at
+
+        # if it's been too long or it's the regularly-scheduled time
+        if (delta > region.delta) or \
             (region.schedule_period == '6hrs' and
-                (now.hour - region.schedule_hour) % 6 == 0) or \
+                (now.hour - region.schedule_hour) % 6 == 0 and
+                delta > timedelta(hours=2)) or \
             (region.schedule_period == 'daily' and
-                region.schedule_hour == now.hour) or \
+                region.schedule_hour == now.hour and
+                delta > timedelta(hours=2)) or \
             (region.schedule_period == 'weekly' and
                 (now.weekday() + 1) % 7 == 0 and
-                region.schedule_hour == now.hour) or \
+                region.schedule_hour == now.hour and
+                delta > timedelta(hours=2)) or \
             (region.schedule_period == 'monthly' and
                 now.day == 1 and
-                region.schedule_hour == now.hour):
+                region.schedule_hour == now.hour and
+                delta > timedelta(hours=2)):
             ExportTaskRunner().run_task(job_uid=region.job.uid)
