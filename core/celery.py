d975a18feaba309b52a62b0ac7e575d521527f09
Seth Fitzsimmons <seth@mojodna.net>
2017-05-01 17:28:00 -0700

    (Re-)load HDX Configuration when workers start
    Initializing the Configuration singleton pre-fork has the side-effect of
    creating a requests session, which, when configured to use keep-alives,
    leads to SSL errors resulting from the same socket being used by
    multiple processes simultaneously.

--- core/celery.py
+++ core/celery.py
@@ -8,9 +8,19 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.project')
 
 from django.conf import settings
+from celery.signals import worker_process_init
 
+from hdx.configuration import Configuration
 
 app = Celery('exports')
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@worker_process_init.connect
+def something(signal, sender):
+    Configuration.create(
+        hdx_site=os.getenv('HDX_SITE', 'demo'),
+        hdx_key=os.getenv('HDX_API_KEY'),
+    )
