4fc7ff1144e74eacd6272a3a6b256c4d1caeed4b
Brandon Liu <bdon@bdon.org>
2017-06-14 21:56:11 +0800

    fix links to export detail

--- ui/app/components/HDXExportRegionForm.jsx
+++ ui/app/components/HDXExportRegionForm.jsx
@@ -265,7 +265,7 @@ export class HDXExportRegionForm extends Component {
     return this.exportRegion.runs.slice(0, 10).map((run, i) => (
       <tr key={i}>
         <td>
-          <a href={`/v3/#/exports/details/${this.exportRegion.job_uid}#${run.uid}`}><FormattedDate value={run.run_at} /> <FormattedTime value={run.run_at} /></a>
+          <a href={`/v3/#/exports/detail/${this.exportRegion.job_uid}#${run.uid}`}><FormattedDate value={run.run_at} /> <FormattedTime value={run.run_at} /></a>
         </td>
         <td>
           {run.status}
@@ -485,7 +485,7 @@ export class HDXExportRegionForm extends Component {
                     </Panel>
                   </Col>
                 </Row>
-                <h3>Run History <small><a href={`/exports/${exportRegion.job_uid}`}>view export details</a></small></h3>
+                <h3>Run History <small><a href={`/v3/#/exports/detail/${exportRegion.job_uid}`}>view export details</a></small></h3>
                 {exportRegion.runs.length > 0
                   ? <Table>
                     <thead>
