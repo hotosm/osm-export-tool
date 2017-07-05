ecea5c10373c1478f215192e609525f6287c7d53
Seth Fitzsimmons <seth@mojodna.net>
2017-07-05 16:17:04 -0700

    Prettier

--- ui/app/components/ExportList.jsx
+++ ui/app/components/ExportList.jsx
@@ -3,40 +3,57 @@ import React, { Component } from 'react';
 import { Col, Row, Table, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import MapListView from './MapListView';
-import { getExports } from '../actions/exportsActions'
-import { Paginator } from './utils'
+import { getExports } from '../actions/exportsActions';
+import { Paginator } from './utils';
 
-
-const ExportTable = ({jobs, selectRegion}) => <tbody>
-  {jobs.map((job,i) => {
-    return <tr key={i}>
-      <td><a href={`#/exports/detail/${job.uid}`}>{job.name}</a></td>
-      <td>{job.description}</td>
-      <td>{job.project}</td>
-      <td>{job.created_at}</td>
-      <td>{job.user.username}</td>
-      <td>
-        <Button>
-          <i className='fa fa-globe'/>
-        </Button>
-      </td>
-    </tr>
-  })}
-</tbody>
+const ExportTable = ({ jobs, selectRegion }) =>
+  <tbody>
+    {jobs.map((job, i) => {
+      return (
+        <tr key={i}>
+          <td>
+            <a href={`#/exports/detail/${job.uid}`}>
+              {job.name}
+            </a>
+          </td>
+          <td>
+            {job.description}
+          </td>
+          <td>
+            {job.project}
+          </td>
+          <td>
+            {job.created_at}
+          </td>
+          <td>
+            {job.user.username}
+          </td>
+          <td>
+            <Button>
+              <i className='fa fa-globe' />
+            </Button>
+          </td>
+        </tr>
+      );
+    })}
+  </tbody>;
 
 export class ExportList extends Component {
   componentDidMount () {
-    this.props.getExports()
+    this.props.getExports();
   }
 
   render () {
-    const { getExports, jobs } = this.props
-    const features = {'features':jobs.items.map((j) => j.simplified_geom),'type':'FeatureCollection'}
+    const { getExports, jobs } = this.props;
+    const features = {
+      features: jobs.items.map(j => j.simplified_geom),
+      type: 'FeatureCollection'
+    };
     return (
-      <Row style={{height: '100%'}}>
-        <Col xs={6} style={{height: '100%', overflowY: 'scroll'}}>
-          <div style={{padding: '20px'}}>
-            <h2 style={{display: 'inline'}}>Exports</h2>
+      <Row style={{ height: '100%' }}>
+        <Col xs={6} style={{ height: '100%', overflowY: 'scroll' }}>
+          <div style={{ padding: '20px' }}>
+            <h2 style={{ display: 'inline' }}>Exports</h2>
             <Table>
               <thead>
                 <tr>
@@ -45,16 +62,16 @@ export class ExportList extends Component {
                   <th>Project</th>
                   <th>Created At</th>
                   <th>Owner</th>
-                  <th></th>
+                  <th />
                 </tr>
               </thead>
-              <ExportTable jobs={jobs.items}/>
+              <ExportTable jobs={jobs.items} />
             </Table>
-            <Paginator collection={jobs} getPage={this.props.getExports}/> 
+            <Paginator collection={jobs} getPage={this.props.getExports} />
           </div>
         </Col>
-        <Col xs={6} style={{height: '100%'}}>
-          <MapListView features={features}/>
+        <Col xs={6} style={{ height: '100%' }}>
+          <MapListView features={features} />
         </Col>
       </Row>
     );
@@ -69,12 +86,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    getExports: (url) => dispatch(getExports(url))
+    getExports: url => dispatch(getExports(url))
   };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ExportList);
-
+export default connect(mapStateToProps, mapDispatchToProps)(ExportList);
