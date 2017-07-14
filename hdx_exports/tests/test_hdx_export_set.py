b0a2fc463359f6771f58888bb5a068e7c2e05352
Brandon Liu <bdon@bdon.org>
2017-07-14 19:49:14 -0400

    fix tests

--- hdx_exports/tests/test_hdx_export_set.py
+++ hdx_exports/tests/test_hdx_export_set.py
@@ -3,7 +3,7 @@
 import json
 import unittest
 from hdx_exports.hdx_export_set import HDXExportSet
-from hdx.configuration import Configuration
+from hdx.hdx_configuration import Configuration
 from feature_selection.feature_selection import FeatureSelection
 from django.contrib.gis.geos import GEOSGeometry
 
