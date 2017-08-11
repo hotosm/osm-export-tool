eb8354109ac309e27dc1f9a940d34373d1de0a81
Seth Fitzsimmons <seth@mojodna.net>
2017-08-11 11:59:02 -0700

    About styling

--- ui/app/components/Home.js
+++ ui/app/components/Home.js
@@ -6,48 +6,43 @@ import { Link } from "react-router-dom";
 
 import { login } from "../actions/meta";
 import { selectIsLoggedIn } from "../selectors";
+import geofabrikLogo from "../images/geofabrik-logo.png";
+import usaidLogo from "../images/usaid-logo.png";
+import hewlettLogo from "../images/hewlett-logo.jpg";
+import pdcLogo from "../images/pdc-txt-logo.png";
 
 const Home = ({ isLoggedIn, login }) =>
   <div className="home">
     <Jumbotron className="hero center">
       <h1 className="centered">
-        <FormattedMessage id="ui.title" defaultMessage="HOT Export Tool" />
+        <FormattedMessage
+          id="ui.title"
+          defaultMessage="Download OpenStreetMap Data"
+        />
       </h1>
       <p className="centered">
-        The Export Tool creates up-to-date OpenStreetMap extracts in various
-        file formats.
+        <FormattedMessage
+          id="ui.about.blurb1"
+          defaultMessage="The Export Tool is an open service that creates customized extracts of up-to-date OSM data in various file formats."
+        />
       </p>
       <p className="centered">
-        <Link to="/exports/new" className="btn btn-danger btn-lg">
+        <FormattedMessage
+          id="ui.about.blurb2"
+          defaultMessage="Download and use the data simply by crediting the OpenStreetMap contributors."
+        />
+      </p>
+      <p className="centered">
+        <Link to="/exports/new" className="btn btn-primary btn-lg">
           <FormattedMessage
             id="ui.home.action"
             defaultMessage="Start Exporting"
           />
         </Link>
       </p>
-      <p className="attribution">
-        <a href="https://www.flickr.com/photos/mementosis/4681191786/">
-          mementosis <i className="fa fa-creative-commons" />{" "}
-          <i className="fa fa-flickr" />
-        </a>
-      </p>
     </Jumbotron>
     <Row>
-      <Col md={4}>
-        <h2>
-          <FormattedMessage
-            id="ui.about.subhead"
-            defaultMessage="Download up-to-date humanitarian maps for GIS analysis, GPS devices and Smartphones."
-          />
-        </h2>
-        <p>
-          <FormattedMessage
-            id="ui.about.overview"
-            defaultMessage="The Export Tool creates OpenStreetMap-derived files that can be used seamlessly for GIS and mobile mapping applications. OSM data is updated at one minute intervals. OSM data can be extracted for any polygonal area and filtered to only relevant tags and features."
-          />
-        </p>
-      </Col>
-      <Col md={4}>
+      <Col md={6} mdOffset={3} className="centered">
         <h2>
           <FormattedMessage
             id="ui.about.title.getting_started"
@@ -56,81 +51,119 @@ const Home = ({ isLoggedIn, login }) =>
         </h2>
         <p>
           <FormattedMessage
-            id="ui.about.getting_started"
-            defaultMessage="Browse through existing exports at our {exportsLink} page without registering an account. An OSM account and valid email address is required to create exports. A guide to the Export Tool is available on our {helpLink} page."
+            id="ui.about.getting_started.blurb"
+            defaultMessage="Sign up for an OSM account to start creating exports. Our {quickStartLink} guide will get you using the tool straight away, or read about the Export Tool in more detail through the {learnLink} page."
             values={{
-              exportsLink: (
-                <Link to="/exports">
-                  <FormattedMessage id="ui.exports" defaultMessage="Exports" />
+              quickStartLink: (
+                <Link to="/learn/quick_start">
+                  <FormattedMessage
+                    id="ui.quick_start"
+                    defaultMessage="Quick Start"
+                  />
                 </Link>
               ),
-              helpLink: (
-                <Link to="/help">
-                  <FormattedMessage id="ui.help" defaultMessage="Help" />
+              learnLink: (
+                <Link to="/learn">
+                  <FormattedMessage id="ui.learn" defaultMessage="Learn" />
                 </Link>
-              ),
-              loginLink: (
-                <a href="/osm/login/openstreetmap">
-                  <FormattedMessage id="ui.login" defaultMessage="Login" />
-                </a>
               )
             }}
           />
         </p>
-        {!isLoggedIn &&
-          <div>
-            <h3>
-              <FormattedMessage
-                id="ui.login_to_osm"
-                defaultMessage="Login to OpenStreetMap"
-              />
-            </h3>
-            <p>
-              If you don't have an OpenStreetMap account you can register for
-              one <a href="http://www.openstreetmap.org/user/new">here</a>.
-            </p>
-          </div>}
       </Col>
-      <Col md={4}>
+    </Row>
+    <Row style={{ backgroundColor: "#f8f8f8", paddingTop: 30 }}>
+      <Col md={6} mdOffset={3} className="centered">
         <h2>
           <FormattedMessage
             id="ui.about.other_resources"
             defaultMessage="Other Resources"
           />
         </h2>
+      </Col>
+    </Row>
+    <Row style={{ backgroundColor: "#f8f8f8", padding: 30 }}>
+      <Col md={4} className="centered">
+        <h3>
+          <FormattedMessage
+            id="ui.about.tasking_manager"
+            defaultMessage="HOT Tasking Manager"
+          />
+        </h3>
+        <p>
+          <FormattedMessage
+            id="ui.about.tasking_manager.blurb"
+            defaultMessage="The {tmLink} is used to coordinate and structure large-scale collaborative mapping in OpenStreetMap."
+            values={{
+              tmLink: (
+                <a href="http://tasks.hotosm.org/">
+                  <FormattedMessage
+                    id="ui.about.tasking_manager.title"
+                    defaultMessage="Tasking Manager"
+                  />
+                </a>
+              )
+            }}
+          />
+        </p>
+      </Col>
+      <Col md={4} className="centered">
+        <h3>
+          <FormattedMessage
+            id="ui.about.osm_analytics"
+            defaultMessage="OSM Analytics"
+          />
+        </h3>
+        <p>
+          <FormattedMessage
+            id="ui.about.other_resources.osma"
+            defaultMessage="The {osmaLink} looks at how the world is mapped over time by OpenStreetMap contributors."
+            values={{
+              osmaLink: (
+                <Link to="http://osm-analytics.org/">
+                  <FormattedMessage
+                    id="ui.about.other_resources.osma.title"
+                    defaultMessage="Analytics Tool"
+                  />
+                </Link>
+              )
+            }}
+          />
+        </p>
+      </Col>
+      <Col md={4} className="centered">
+        <h3>
+          <FormattedMessage
+            id="ui.about.hdx_platform"
+            defaultMessage="HDX Platform"
+          />
+        </h3>
         <p>
           <FormattedMessage
             id="ui.about.other_resources.hdx"
-            defaultMessage="The {hdxLink} hosts exports for popular regions, updated at regular intervals."
+            defaultMessage="The {hdxLink} hosts OpenStreetMap exports for select countries, updated at regular intervals."
             values={{
               hdxLink: (
                 <Link to="https://data.humdata.org/">
                   <FormattedMessage
                     id="ui.about.other_resources.hdx_link"
-                    defaultMessage="Humanitarian Data Exchange (HDX)"
+                    defaultMessage="Humanitarian Data Exchange"
                   />
                 </Link>
               )
             }}
           />
         </p>
-        <p>
+      </Col>
+    </Row>
+    <Row>
+      <Col md={6} mdOffset={3} className="centered" style={{ paddingTop: 30 }}>
+        <h3>
           <FormattedMessage
-            id="ui.about.other_resources.tm"
-            defaultMessage="The {tmLink} coordinates humanitarian mapping efforts."
-            values={{
-              tmLink: (
-                <Link to="http://tasks.hotosm.org/">
-                  <FormattedMessage
-                    id="ui.about.other_resources.tm_link"
-                    defaultMessage="HOT Tasking Manager"
-                  />
-                </Link>
-              )
-            }}
+            id="ui.about.title.license_information"
+            defaultMessage="License Information"
           />
-        </p>
-        <h3>License Information</h3>
+        </h3>
         <p>
           <FormattedMessage
             id="ui.about.license"
@@ -147,6 +180,74 @@ const Home = ({ isLoggedIn, login }) =>
             }}
           />
         </p>
+      </Col>
+    </Row>
+    <Row>
+      <Col md={6} mdOffset={3} className="centered" style={{ paddingTop: 30 }}>
+        <h3>
+          <FormattedMessage
+            id="ui.about.title.help"
+            defaultMessage="Help Improve the Tool"
+          />
+        </h3>
+        <p>
+          <FormattedMessage
+            id="ui.about.help"
+            defaultMessage="Check out the {githubLink} repository to learn more about the design and how to get involved in the project."
+            values={{
+              githubLink: (
+                <a href="https://github.com/hotosm/osm-export-tool2">
+                  <FormattedMessage
+                    id="ui.about.github"
+                    defaultMessage="GitHub"
+                  />
+                </a>
+              )
+            }}
+          />
+        </p>
+      </Col>
+    </Row>
+    <Row>
+      <Col md={6} mdOffset={3} className="centered" style={{ paddingTop: 30 }}>
+        <h3>
+          <FormattedMessage
+            id="ui.about.title.partners_and_contributors"
+            defaultMessage="Partners and Contributors"
+          />
+        </h3>
+        <p>
+          <FormattedMessage
+            id="ui.about.partners and contributors"
+            defaultMessage="The Export Tool is made possible by many organizations and individuals who contribute time and resources to the project."
+          />
+        </p>
+      </Col>
+    </Row>
+    <Row style={{ paddingTop: 30, paddingBottom: 30 }}>
+      <Col md={4} mdOffset={2} className="centered">
+        <a href="http://www.pdc.org/">
+          <img src={pdcLogo} width={400} />
+        </a>
+      </Col>
+      <Col md={5}>
+        <a href="https://www.usaid.gov/">
+          <img src={usaidLogo} width={400} />
+        </a>
+      </Col>
+    </Row>
+    <Row style={{ paddingTop: 30, paddingBottom: 30 }}>
+      <Col md={6} mdOffset={3} className="centered">
+        <a href="http://www.hewlett.org/">
+          <img src={hewlettLogo} width={400} />
+        </a>
+      </Col>
+    </Row>
+    <Row style={{ paddingTop: 30, paddingBottom: 50 }}>
+      <Col md={6} mdOffset={3} className="centered">
+        <a href="https://www.geofabrik.de/">
+          <img src={geofabrikLogo} width={400} />
+        </a>
       </Col>
     </Row>
   </div>;
