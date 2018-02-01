5cae2a640dbb7c5fcd3cd77ef2345e0117f9f411
Brandon Liu <bdon@bdon.org>
2018-02-01 14:23:41 -0500

    fix reporting of overpass timestamp if more than 1 day behind

--- ops/report_dd_metrics.py
+++ ops/report_dd_metrics.py
@@ -11,5 +11,5 @@ f = urllib2.urlopen(urllib2.Request('http://localhost:6080/api/timestamp'))
 timestamp_str = f.read().split('\n')[0]
 parsed = datetime.datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%M:%SZ")
 # note: this assumes the system time is UTC, because overpass timestamp is UTC
-replication_seconds_behind = (now - parsed).seconds
+replication_seconds_behind = (now - parsed).total_seconds()
 statsd.gauge("overpass.replication_seconds_behind",replication_seconds_behind)
