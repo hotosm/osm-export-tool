942809a196179e00cfde0f37bafd85287604a635
Brandon Liu <bdon@bdon.org>
2019-06-29 18:34:57 -0400

    packer provision downloads and installs prebuilt binaries for Garmin, Maps.ME, OsmAnd [#316]

--- ops/packer/provision.sh
+++ ops/packer/provision.sh
@@ -31,5 +31,26 @@ python-gdal \
 spatialite-bin \
 unzip \
 yarn \
-zip
+zip \
+nginx \
+qt5-default
 apt clean
+
+wget https://hotosm-export-tool.s3.amazonaws.com/mkgmap-r3890.zip
+unzip mkgmap-r3890.zip -d /usr/local
+mv /usr/local/mkgmap-r3890 /usr/local/mkgmap
+rm mkgmap-r3980.zip
+
+wget https://hotosm-export-tool.s3.amazonaws.com/splitter-r583.zip
+unzip splitter-r583.zip -d /usr/local
+mv /usr/local/splitter-r583 /usr/local/splitter
+rm splitter-r583.zip
+
+wget https://hotosm-export-tool.s3.amazonaws.com/OsmAndMapCreator-main.zip
+mkdir /usr/local/OsmAndMapCreator
+unzip OsmAndMapCreator-main.zip -d /usr/local/OsmAndMapCreator
+rm OsmAndMapCreator-main.zip
+
+wget https://hotosm-export-tool.s3.amazonaws.com/omim-build-release-9ac133b.tgz
+sudo tar -xzf omim-build-release-9ac133b.tgz -C /usr/local/
+sudo mv /usr/local/omim-build-release /usr/local/omim
