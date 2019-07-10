cd0098a0e27eaf8598261bedf95b97d7be97d337
Brandon Liu <bdon@bdon.org>
2019-07-09 21:44:51 -0400

    azure config changes; packer VM doesn't have ubuntu user [#315]

--- ops/packer/overpass_provision.sh
+++ ops/packer/overpass_provision.sh
@@ -11,6 +11,6 @@ rm osm-3s-v0.7.55.tgz
 mv osm-3s_v0.7.55/ /home/overpass/
 chown -R overpass:overpass /home/overpass/osm-3s_v0.7.55/
 mv /tmp/motd /etc/motd
-mv /tmp/cloudwatch_metrics.py /home/ubuntu/cloudwatch_metrics.py
-chown ubuntu:ubuntu /home/ubuntu/cloudwatch_metrics.py
-chmod +x /home/ubuntu/cloudwatch_metrics.py
+mv /tmp/cloudwatch_metrics.py /home/overpass/cloudwatch_metrics.py
+chown overpass:overpass /home/overpass/cloudwatch_metrics.py
+chmod +x /home/overpass/cloudwatch_metrics.py
