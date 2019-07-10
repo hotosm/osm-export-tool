c3fccdaa089e31d201fe077d14be5ad0832c6a53
Brandon Liu <bdon@bdon.org>
2019-07-09 20:48:53 -0400

    more overpass config [#315]

--- ops/packer/overpass_provision.sh
+++ ops/packer/overpass_provision.sh
@@ -1,7 +1,13 @@
 apt update
 apt upgrade -y
-apt install -y nginx fcgiwrap
+apt install -y nginx fcgiwrap python3-pip
+pip3 install boto3
+
+adduser --disabled-password --gecos "" overpass
 
 wget https://hotosm-export-tool.s3.amazonaws.com/osm-3s-v0.7.55.tgz
 tar -xzf osm-3s-v0.7.55.tgz
 rm osm-3s-v0.7.55.tgz
+mv osm-3s_v0.7.55/ /home/overpass/
+chown -R overpass:overpass /home/overpass/osm-3s_v0.7.55/
+mv /tmp/motd /etc/motd
