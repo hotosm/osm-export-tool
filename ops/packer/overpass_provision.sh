1a9d0a355cf0a47e2f14ae74875b4346be9126c5
Brandon Liu <bdon@bdon.org>
2019-07-10 12:59:41 -0400

    configure nginx.conf [#315]

--- ops/packer/overpass_provision.sh
+++ ops/packer/overpass_provision.sh
@@ -10,7 +10,8 @@ tar -xzf osm-3s-v0.7.55.tgz
 rm osm-3s-v0.7.55.tgz
 mv osm-3s_v0.7.55/ /home/overpass/
 chown -R overpass:overpass /home/overpass/osm-3s_v0.7.55/
-mv /tmp/motd /etc/motd
-mv /tmp/cloudwatch_metrics.py /home/overpass/cloudwatch_metrics.py
 chown overpass:overpass /home/overpass/cloudwatch_metrics.py
 chmod +x /home/overpass/cloudwatch_metrics.py
+mv /tmp/motd /etc/motd
+mv /tmp/cloudwatch_metrics.py /home/overpass/cloudwatch_metrics.py
+mv /tmp/nginx.conf /etc/nginx.conf
