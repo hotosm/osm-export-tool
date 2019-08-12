dfc608c8c1c09b26bfcade711decd4d6cc1116fe
Brandon Liu <bdon@bdon.org>
2019-08-12 15:51:58 -0400

    set output header to timestamp number [#322]

--- jobs/secondary_pipeline.py
+++ jobs/secondary_pipeline.py
@@ -20,25 +20,34 @@ if not os.path.isfile(planet):
 	subprocess.call(['wget','-O',planet,PLANET_OSM_PBF])
 
 fileinfo = json.loads(subprocess.check_output(['osmium','fileinfo','-j',planet]))
-timestamp = fileinfo['header']['option']['osmosis_replication_timestamp']
-timestamp = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
-timestamp = timestamp.replace(tzinfo=timezone.utc)
-logging.warning("Timestamp is {0}".format(timestamp))
 
-daily = server.ReplicationServer('https://planet.openstreetmap.org/replication/day')
-seqnum = daily.timestamp_to_sequence(timestamp)
+option = fileinfo['header']['option']
+
+if 'osmosis_replication_sequence_number' in option:
+	seqnum = option['osmosis_replication_sequence_number']
+else:
+	timestamp = fileinfo['header']['option']['osmosis_replication_timestamp']
+	timestamp = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
+	timestamp = timestamp.replace(tzinfo=timezone.utc)
+	logging.warning("Timestamp is {0}".format(timestamp))
+	daily = server.ReplicationServer('https://planet.openstreetmap.org/replication/day')
+	seqnum = daily.timestamp_to_sequence(timestamp)
+
 logging.warning("Seqnum is {0}".format(seqnum))
 
-# get the latest
+latest = daily.get_state_info().sequence
 
 try:
-	os.mkdir(os.path.join(workdir,'tmp'),0755)
+	os.mkdir(os.path.join(workdir,'tmp'))
 except:
 	pass
 
-for i in range(seqnum+1,daily.get_state_info().sequence):
+for i in range(seqnum+1,latest):
 	subprocess.call(['wget','-O',os.path.join(workdir,'tmp','{0}.osc.gz'.format(i)),daily.get_diff_url(i)])
-	print(i)
 
-subprocess.call(['osmium','merge-changes','--simplify','work/tmp/*.osc.gz','-o',os.path.join(workdir,'merged-changes.osc.gz')])
-subprocess.call(['osmium','apply-changes',planet,os.path.join(workdir,'merged-changes.osc.gz','-o',os.path.join(workdir,'planet-updated.osm.pbf'))])
+g = glob.glob(os.path.join(workdir,'tmp','*.osc.gz'))
+subprocess.call(['osmium','merge-changes','--overwrite','--simplify',*g,'-o',os.path.join(workdir,'merged-changes.osc.gz')])
+subprocess.call(['osmium','apply-changes','--output-header',"'osmosis_replication_sequence_number={0}'".format(latest),planet,os.path.join(workdir,'merged-changes.osc.gz','-o',os.path.join(workdir,'planet-updated.osm.pbf'))])
+
+# move the old one into the new one
+# remove the tmp directory
