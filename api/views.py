e5bbd83bd3ce8bd089edac5439866316a73db575
itskshitiz321 <skshitizraj@gmail.com>
2022-05-24 10:29:34 +0545

    added raise error

--- api/views.py
+++ api/views.py
@@ -55,8 +55,8 @@ DIR = os.path.dirname(os.path.abspath(__file__))
 try:
     idx = index.Rtree(os.path.join(DIR,'reverse_geocode'))
 except:
-    pass
-    # raise ImportError("Can not read indexes")
+    # pass
+    raise ImportError("Can not read indexes")
 def bbox_to_geom(s):
     try:
         return GEOSGeometry(Polygon.from_bbox(s.split(',')), srid=4326)
