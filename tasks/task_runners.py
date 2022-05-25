c3ffcc9e97904e493944735039f3b071b5f1aa9e
itskshitiz321 <skshitizraj@gmail.com>
2022-05-25 17:24:12 +0545

    added use only galaxy option on task runners

--- tasks/task_runners.py
+++ tasks/task_runners.py
@@ -320,15 +320,22 @@ def run_task(run_uid,run,stage_dir,download_dir):
         kml = None
         geojson=None
         tabular_outputs = []
+        
+        use_only_galaxy = False
+        galaxy_supported_outputs = ['geojson']
+
+        if galaxy_supported_outputs == list(export_formats):
+            use_only_galaxy=True
+
         if 'geopackage' in export_formats:
             geopackage = tabular.Geopackage(join(stage_dir,valid_name),mapping)
             tabular_outputs.append(geopackage)
             start_task('geopackage')
 
         if 'shp' in export_formats:
-            shp = Galaxy(settings.GALAXY_API_URL,geom,mapping=mapping,file_name=valid_name)
-            # shp = tabular.Shapefile(join(stage_dir,valid_name),mapping)
-            # tabular_outputs.append(shp)
+            # shp = Galaxy(settings.GALAXY_API_URL,geom,mapping=mapping,file_name=valid_name)
+            shp = tabular.Shapefile(join(stage_dir,valid_name),mapping)
+            tabular_outputs.append(shp)
             start_task('shp')
         
         if 'geojson' in export_formats:
@@ -343,16 +350,18 @@ def run_task(run_uid,run,stage_dir,download_dir):
             h = tabular.Handler(tabular_outputs,mapping,polygon_centroid=polygon_centroid)
             source = OsmiumTool('osmium',settings.PLANET_FILE,geom,join(stage_dir,'extract.osm.pbf'),tempdir=stage_dir, mapping=mapping)
         else:
-            h = tabular.Handler(tabular_outputs,mapping,clipping_geom=geom,polygon_centroid=polygon_centroid)
             mapping_filter = mapping
             if job.unfiltered:
-                mapping_filter = None  
-            source = Overpass(settings.OVERPASS_API_URL,geom,join(stage_dir,'overpass.osm.pbf'),tempdir=stage_dir,use_curl=True,mapping=mapping_filter)
-        LOG.debug('Source start for run: {0}'.format(run_uid))
-        source_path = source.path()
-        LOG.debug('Source end for run: {0}'.format(run_uid))
+                mapping_filter = None 
+            if use_only_galaxy == False : 
+                h = tabular.Handler(tabular_outputs,mapping,clipping_geom=geom,polygon_centroid=polygon_centroid)
+                source = Overpass(settings.OVERPASS_API_URL,geom,join(stage_dir,'overpass.osm.pbf'),tempdir=stage_dir,use_curl=True,mapping=mapping_filter)
+        if use_only_galaxy == False :        
+            LOG.debug('Source start for run: {0}'.format(run_uid))
+            source_path = source.path()
+            LOG.debug('Source end for run: {0}'.format(run_uid))
 
-        h.apply_file(source_path, locations=True, idx='sparse_file_array')
+            h.apply_file(source_path, locations=True, idx='sparse_file_array')
 
         bundle_files = []
         
@@ -364,8 +373,12 @@ def run_task(run_uid,run,stage_dir,download_dir):
             finish_task('geopackage',[zipped])
 
         if shp:
-            response_back=shp.fetch('shp')
-            finish_task('shp',response_back=response_back)
+            shp.finalize()
+            zipped = create_package(join(download_dir,valid_name + '_shp.zip'),shp.files,boundary_geom=geom)
+            bundle_files += shp.files
+            finish_task('shp',[zipped])
+            # response_back=shp.fetch('shp')
+            # finish_task('shp',response_back=response_back)
 
         if geojson :
             response_back=geojson.fetch('GeoJSON')
