94f159c07dcd9cc5a7a8204408ec6cd64e0436cc
itskshitiz321 <skshitizraj@gmail.com>
2022-09-21 16:53:57 +0545

    changed create url

--- ui/app/components/NavBar.js
+++ ui/app/components/NavBar.js
@@ -22,7 +22,7 @@ const NavBar = ({ isLoggedIn, login, logout }) => (
     <Nav className="pull-right">
       <RequirePermission>
         <li>
-          <NavLink to="https://status.hotosm.org">
+          <NavLink to="/exports/new">
             <FormattedMessage id="ui.create" defaultMessage="Create" />
           </NavLink>
         </li>
