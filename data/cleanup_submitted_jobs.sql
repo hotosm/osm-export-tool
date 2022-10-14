850e6f076116f3477611b319a3c8f4c5c3839816
itskshitiz321 <skshitizraj@gmail.com>
2022-10-14 14:02:26 +0545

    added ;

--- data/cleanup_submitted_jobs.sql
+++ data/cleanup_submitted_jobs.sql
@@ -1,1 +1,1 @@
-update export_runs set status = 'FAILED' where created_at < NOW() - INTERVAL '1 days' and status = 'SUBMITTED'
+update export_runs set status = 'FAILED' where created_at < NOW() - INTERVAL '1 days' and status = 'SUBMITTED';
