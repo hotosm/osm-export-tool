a56c0286263a2329970733ee1e42e954119605e7
kshitijrajsharma <skshitizraj@gmail.com>
2023-02-18 14:17:44 +0545

    Update for scheduling jobs on minutely basis check

--- jobs/management/commands/schedule.py
+++ jobs/management/commands/schedule.py
@@ -13,43 +13,30 @@ class Command(BaseCommand):
     help = 'Displays current time'
 
     def handle(self, *args, **kwargs):
+
         now = timezone.now()
 
-        for regioncls in [HDXExportRegion, PartnerExportRegion]: 
+
+        for regioncls in [HDXExportRegion, PartnerExportRegion]:
             for region in regioncls.objects.exclude(schedule_period='disabled'):
+                schedule_match = False
                 last_run = region.job.runs.last()
                 if last_run:
-                    last_run_at = last_run.created_at
-                else:
-                    last_run_at = datetime.fromtimestamp(0, pytz.timezone('UTC'))
+                    last_run_status=last_run.status
+                    if last_run_status in ['RUNNING','SUBMITTED']:
+                        continue
+                if region.schedule_period == '6hrs':
+                    schedule_match = (now.hour - region.schedule_hour) % 6 == 0
+                elif region.schedule_period == 'daily':
+                    schedule_match = region.schedule_hour == now.hour
+                elif region.schedule_period == 'weekly':
+                    schedule_match = (now.weekday() + 1) % 7 == 0 and region.schedule_hour == now.hour
+                elif region.schedule_period == '2wks':
+                    schedule_match = now.day in (15, 1) and region.schedule_hour == now.hour
+                elif region.schedule_period == '3wks':
+                    schedule_match = now.day == 21 and region.schedule_hour == now.hour
+                elif region.schedule_period == 'monthly':
+                    schedule_match = now.day == 1 and region.schedule_hour == now.hour
 
-                delta = now - last_run_at
-
-                # if it's been too long or it's the regularly-scheduled time
-                if (delta > region.delta) or \
-                    (region.schedule_period == '6hrs' and
-                        (now.hour - region.schedule_hour) % 6 == 0 and
-                        delta > timedelta(hours=2)) or \
-                    (region.schedule_period == 'daily' and
-                        region.schedule_hour == now.hour and
-                        delta > timedelta(hours=2)) or \
-                    (region.schedule_period == 'weekly' and
-                        (now.weekday() + 1) % 7 == 0 and
-                        region.schedule_hour == now.hour and
-                        delta > timedelta(hours=2)) or \
-                    (region.schedule_period == '2wks' and
-                        now.day in (15, 1) and
-                        region.schedule_hour == now.hour and
-                        delta > timedelta(hours=2)) or \
-                    (region.schedule_period == '3wks' and
-                        now.day == 21 and
-                        region.schedule_hour == now.hour and
-                        delta > timedelta(hours=2)) or \
-                    (region.schedule_period == 'monthly' and
-                        now.day == 1 and
-                        region.schedule_hour == now.hour and
-                        delta > timedelta(hours=2)):
+                if schedule_match:
                     ExportTaskRunner().run_task(job_uid=region.job.uid,ondemand=False)
-
-
-
